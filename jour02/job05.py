ce33150c0364d23d882d737cf5ae3b1fafc75c1c
adam-messaoudi <adam.messaoudi@laplateforme.io>
2024-02-02 10:27:00 +0100

    ajout job05 et job06

--- jour02/job05.py
+++ jour02/job05.py
@@ -1,6 +1,7 @@
 import mysql.connector
 
 # Informations de connexion à la base de données
+
 config = {
     'host': 'localhost',
     'user': 'root',
@@ -17,21 +18,23 @@ try:
         # Création d'un curseur pour exécuter des requêtes SQL
         cursor = connection.cursor()
 
-        # Exécution de la requête pour récupérer l'ensemble des étudiants
-        query = "SELECT * FROM etudiant"
+        # Exécution de la requête pour calculer la superficie totale des étages
+        query = "SELECT SUM(superficie) AS superficie_totale FROM etage"
         cursor.execute(query)
 
         # Récupération des résultats
-        students = cursor.fetchall()
+        result = cursor.fetchone()
 
-        # Affichage des résultats en console
-        for student in students:
-            print(student)
+        # Affichage du résultat en console
+        superficie_totale = result[0]
+        print(f"La superficie de La Plateforme est de {superficie_totale} m2.")
 
 except mysql.connector.Error as err:
     print(f"Erreur de connexion à la base de données: {err}")
+
 finally:
-    # Fermeture de la connexion
+    # Fermeture du curseur et de la connexion
     if 'connection' in locals() and connection.is_connected():
+        cursor.close()
         connection.close()
         print("Connexion fermée.")
--- jour02/job06.py
+++ jour02/job06.py
@@ -17,21 +17,23 @@ try:
         # Création d'un curseur pour exécuter des requêtes SQL
         cursor = connection.cursor()
 
-        # Exécution de la requête pour récupérer l'ensemble des étudiants
-        query = "SELECT * FROM etudiant"
+        # Exécution de la requête pour calculer la capacité totale des salles
+        query = "SELECT SUM(capacite) AS capacite_totale FROM salle"
         cursor.execute(query)
 
         # Récupération des résultats
-        students = cursor.fetchall()
+        result = cursor.fetchone()
 
-        # Affichage des résultats en console
-        for student in students:
-            print(student)
+        # Affichage du résultat en console
+        capacite_totale = result[0]
+        print(f"La capacité totale des salles est de : {capacite_totale}")
 
 except mysql.connector.Error as err:
     print(f"Erreur de connexion à la base de données: {err}")
+
 finally:
-    # Fermeture de la connexion
+    # Fermeture du curseur et de la connexion
     if 'connection' in locals() and connection.is_connected():
+        cursor.close()
         connection.close()
         print("Connexion fermée.")
